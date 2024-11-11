d8898b00e390218fbaac969bff102bda6b613545
akash100a <arakash968786@gmail.com>
2024-11-11 20:52:23 +0600

    Update1

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Bannar from './component/layers/Bannar';
 import NewProduct from './component/layers/NewProduct';
 import CustomBuild from './component/layers/CustomBuild';
+import Zip from './component/layers/Zip';
 
 const page = () => {
 
@@ -9,6 +10,7 @@ const page = () => {
     <>
       <Bannar />
       <NewProduct className='my-5'/>
+      <Zip />
       <CustomBuild className='my-5'/>
     </>
   )

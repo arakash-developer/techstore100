0c07ad8548f5fb2267d6f6813f1fa7b7be093297
akash100a <arakash968786@gmail.com>
2024-11-11 22:23:38 +0600

    update

--- app/products/details/[id]/page.jsx
+++ app/products/details/[id]/page.jsx
@@ -9,14 +9,26 @@ export async function generateMetadata({ params }) {
     let res = await getAllData();
     let newdata = res.find((item) => (item.id == id))
     let brand = "TechStore- "
+    let weburl = "https://techstore100.vercel.app"
+    let mainurl = `${weburl}/${id}`
     return {
         title: brand + newdata.title,
         description: newdata.description,
+        keywords: newdata.tags,
+        openGraph: {
+            type: 'website',
+            url: mainurl,
+            title: brand + newdata.title,
+            description: newdata.description,
+            images: newdata.images,
+            site_name: 'TechStore',
+            locale: 'en_US',
+        }
     }
 }
 
 const page = async ({ params }) => {
-    const {id} = await params;
+    const { id } = await params;
     let res = await getAllData();
     let newdata = res.filter((item) => (item.id == id))
     let Allcomments = newdata.map((item) => {
@@ -27,7 +39,7 @@ const page = async ({ params }) => {
             <PreviewImage newdata={newdata} />
 
             {
-                Allcomments[0].map((item,index) => (
+                Allcomments[0].map((item, index) => (
                     <div key={index} className="">
                         <h2>{item.id}</h2>
                         <h2>{item.comment}</h2>

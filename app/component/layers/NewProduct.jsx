be61a7353ac8535fb08a0faf7faea1355010a6c7
akash100a <arakash968786@gmail.com>
2024-11-12 19:43:08 +0600

    a

--- app/component/layers/NewProduct.jsx
+++ app/component/layers/NewProduct.jsx
@@ -43,7 +43,7 @@ const NewProduct = ({ className }) => {
     arrows: true,
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 9000,
+    autoplaySpeed: 30000,
     slidesToShow: 6,
     slidesToScroll: 1,
     nextArrow: <SampleNextArrow check={`${loading ? "!hidden" : "!flex"}`} />,

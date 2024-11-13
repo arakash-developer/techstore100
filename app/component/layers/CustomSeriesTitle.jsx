aa20f2e55e9403e8717e4467f6b7fb0fe5491629
akash100a <arakash968786@gmail.com>
2024-11-13 14:48:17 +0600

    a

--- app/component/layers/CustomSeriesTitle.jsx
+++ app/component/layers/CustomSeriesTitle.jsx
@@ -4,7 +4,7 @@ import Container from './Container'
 const CustomSeriesTitle = ({s1,s2,s3,s4}) => {
     return (
         <Container>
-            <div className='flex gap-x-6 items-center'>
+            <div className='flex gap-x-6 items-center cursor-pointer'>
                 <h3 className='font-semibold underline text-base leading-[140%] text-center text-[var(--color---7)]'>{s1}</h3>
                 <h3 className='font-semibold text-base leading-[140%] text-center text-[var(--color---20)] capitalize'>{s2}</h3>
                 <h3 className='font-semibold text-base leading-[140%] text-center text-[var(--color---20)] capitalize'>{s3}</h3>

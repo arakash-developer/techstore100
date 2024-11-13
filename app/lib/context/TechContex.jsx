214b01d4595e246379d17b47bb210b0698cf80db
akash100a <arakash968786@gmail.com>
2024-11-14 01:28:54 +0600

    a

--- app/lib/context/TechContex.jsx
+++ app/lib/context/TechContex.jsx
@@ -2,7 +2,7 @@ import { createContext,useState } from "react";
 export let Contex = createContext();
 let TechContex = ({children}) =>{
     let [page,setPage] = useState(1)
-    let [limit,setLimit] = useState(2)
+    let [limit,setLimit] = useState(6)
     return(
         <Contex.Provider value={{limit,setLimit,page,setPage}}>
             {children}

aecb1d6038b75b444d5776fb78d81848a862352c
akash100a <arakash968786@gmail.com>
2024-11-14 23:54:22 +0600

    blog

--- app/component/layers/AllBlog.jsx
+++ app/component/layers/AllBlog.jsx
@@ -1,35 +1,23 @@
 import Container from './Container'
 import BlogItem from './BlogItem'
+import blogData from '@/app/lib/blogData'
 
 
-const AllBlog = () => {
+const AllBlog = async() => {
+  let res = await blogData()
   return (
     <>
-        <Container>
-          <h2 className='font-semibold text-[1.38rem] leading-7 mb-6 text-[var(--color---7)]'>Follow us on Instagram for News, Offers & More</h2>
-    <div className="grid grid-cols-12 gap-x-2 gap-y-5 w-full ">
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-        <BlogItem className='' />
-       
-    </div>
-</Container>
+      <Container>
+        <h2 className='font-semibold text-[1.38rem] leading-7 mb-6 text-[var(--color---7)]'>Follow us on Instagram for News, Offers & More</h2>
+        <div className="grid grid-cols-12 gap-x-2 gap-y-5 w-full ">
+          {
+            res.map((item)=>(
+              <BlogItem key={item.id} img={item.img} date={`0${item.id}.0${item.userId}.2024`} body={item.title+item.body} className='' />
+            ))
+          }
+        
+        </div>
+      </Container>
     </>
   )
 }
--- app/component/layers/BlogItem.jsx
+++ app/component/layers/BlogItem.jsx
@@ -1,17 +1,16 @@
 import React from 'react'
-import Blog1 from '@/public/blog1.png'
 import Image from 'next/image'
 
-const BlogItem = ({ className }) => {
+const BlogItem = ({ className,body,date,img }) => {
     return (
         <>
             <div className={`w-full h-[422px] sm:h-[322px] col-span-12 sm:col-span-6 md:col-span-4 lg:col-span-3 xl:col-span-2 ${className}`}>
                 <div className="w-full h-[250px] sm:h-[150px]">
-                    <Image className='w-full h-full object-cover' src={Blog1} alt={Blog1} />
+                    <Image width={225} height={150} className='w-full h-full object-cover' src={img} alt={img} />
                 </div>
                     <div className="py-4 px-5 w-full">
-                        <p className='font-normal w-full sm:max-w-[185px] text-xs text-center text-[var(--color---7)]'>If you’ve recently made a desktop PC or laptop purchase, you might want to consider adding peripherals to enhance your home office setup, your gaming rig, or your business workspace...</p>
-                        <p className='font-normal text-[0.63rem] mt-3 text-center text-[var(--color---5)]'>01.09.2020</p>
+                        <p className='capitalize font-normal w-full sm:max-w-[185px] text-xs text-center text-[var(--color---7)]'>{body}</p>
+                        <p className='font-normal text-[0.63rem] mt-3 text-center text-[var(--color---5)]'>{date}</p>
                     </div>
 
             </div>

c38fcf6eb7d065299d0912424caaa785c3571758
akash100a <arakash968786@gmail.com>
2024-12-15 09:37:30 +0600

    uu

---
+++ app/testing/A.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const a = () => {
+  return (
+    <div>default</div>
+  )
+}
+
+export const Ar = () => {
+  return (
+    <div>aaaaaa</div>
+  )
+}
+
+export default a

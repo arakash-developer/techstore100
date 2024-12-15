9a2fe9d1137ed85ee95bf21c2ab83fe6a4d2c0c3
akash100a <arakash968786@gmail.com>
2024-12-15 08:01:34 +0600

    kk

---
+++ a.js
@@ -0,0 +1,56 @@
+// let ff = (a,b) =>{
+//     console.log(ff.xyz);
+// }
+
+// // console.log(ff);
+
+// ff()
+// ff.xyz = 400
+// ff.xyz = 200
+// ff()
+
+
+// const number = [1,2,3,4,5,6,7,8,9,10]
+// number[100]=500
+// console.log(number);
+
+// let a = 100
+
+// console.log(typeof (typeof (a)));
+
+
+const number = [1,2,3,4,5,6,7,8,9,10]
+const number2 = number
+number2.push(1000)
+console.log(number2);
+console.log(number);
+
+// let b = [...'akash']
+// console.log(b);
+
+// let c = '300'
+// let oo = parseInt(c)
+// console.log(oo);
+// console.log(typeof(oo));
+
+// let c = '100+3+20'
+// let oo = parseInt(c)
+// console.log(oo);
+// console.log(typeof(oo));
+
+// let c = '1M7F'
+// let oo = parseInt(c)
+// console.log(oo);
+// console.log(typeof(oo));
+
+// let a = undefined
+// console.log(typeof a);
+
+// let c = '1M7F'
+// let oo = parseInt(c)
+// console.log(oo);
+// console.log(typeof(oo));
+
+
+// console.log(isNaN(100));
+// console.log(isNaN('akash'));

cf2cf46724d98d272603f59a1c79a3b604011712
akash100a <arakash968786@gmail.com>
2024-12-16 08:06:12 +0600

    gh

--- app/testing/page.jsx
+++ app/testing/page.jsx
@@ -3,10 +3,34 @@ import Container from "@/app/component/layers/Container";
 import A from "./A";
 import { Ar as Arc } from "./A";
 
+
 let cal = (b, c) => {
   let a = b + c;
   return a;
 };
+
+let a = [
+  {
+    a: 1,
+    sub:"bangla",
+    number:200,
+  },
+  {
+    a: 2,
+    sub:"english",
+    number:300
+  },
+  {
+    a: 3,
+    sub:"math",
+    number:400
+  },
+  {
+    a: 4,
+    sub:"bangla",
+    number:500
+  }
+]
 
 const page = () => {
   let person = {
@@ -23,6 +47,7 @@ let style = {
     fontSize: "30px",
 }
 
+let item = a.map((item) => (<li key={item.number}>{item.sub}</li>))
 let making = (
     <>
       <h1 style={style}>Hellow</h1>
@@ -39,6 +64,7 @@ let making = (
         <Arc />
         <h1>Cal-{cal(2, 101)}</h1>
         <div>{making}</div>
+        <div className="">{item}</div>
       </Container>
     </div>
   );

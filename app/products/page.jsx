c6df4cce96e8cea20c5a252c864a124244f95c34
akash100a <arakash968786@gmail.com>
2025-01-10 18:58:44 +0600

    upp

--- app/products/page.jsx
+++ app/products/page.jsx
@@ -1,38 +1,48 @@
+// import React from 'react'
+// import getAllData from '../lib/getAllData';
+// import Container from '../component/layers/Container';
+// import Link from 'next/link';
+
+// export async function generateMetadata() {
+//     let res = await getAllData();
+//     let brand = "TechStore- "
+//     return {
+//         title: brand + "All Products " + res.map((item) => (
+//             item.title
+//         )),
+//         description: res.map((item) => (
+//             item.description
+//         )),
+//     }
+// }
+
+// const page = async () => {
+//     let res = await getAllData();
+//     return (
+//         <Container>
+//             <div className='my-52'>
+//                 {
+//                     res.map((item) => (
+//                         <div className="" key={item.id}>
+//                             <Link href={`/product/details/${item.id}`}>
+//                                 <h1>{item.title}</h1>
+//                             </Link>
+//                         </div>
+//                     ))
+//                 }
+//             </div>
+//         </Container>
+//     )
+// }
+
+// export default page
+
 import React from 'react'
-import getAllData from '../lib/getAllData';
-import Container from '../component/layers/Container';
-import Link from 'next/link';
 
-export async function generateMetadata() {
-    let res = await getAllData();
-    let brand = "TechStore- "
-    return {
-        title: brand + "All Products " + res.map((item) => (
-            item.title
-        )),
-        description: res.map((item) => (
-            item.description
-        )),
-    }
-}
-
-const page = async () => {
-    let res = await getAllData();
-    return (
-        <Container>
-            <div className='my-52'>
-                {
-                    res.map((item) => (
-                        <div className="" key={item.id}>
-                            <Link href={`/product/details/${item.id}`}>
-                                <h1>{item.title}</h1>
-                            </Link>
-                        </div>
-                    ))
-                }
-            </div>
-        </Container>
-    )
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
 export default page
